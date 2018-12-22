d2593d67462de0d80d6efc9fc306332ec32242a5
Abhijat Malviya <malviya.abhijat@gmail.com>
2018-12-22 20:58:09 +0530

    adds handlers to post payload and check status

---
+++ src/handlers.rs
@@ -0,0 +1,31 @@
+use actix_web::*;
+
+use crate::api::FilteredResponse;
+use crate::api::KeysRequest;
+use crate::bloom_filter::AppState;
+
+pub fn push_keys((request, payload): (HttpRequest<AppState>, Json<KeysRequest>)) -> Result<HttpResponse> {
+    let items = &payload.keys;
+
+    let mut filter = request.state().filter.lock().unwrap();
+    items.iter().for_each(|item| filter.add(item));
+
+    Ok(HttpResponse::Ok()
+        .json(
+            format!("added {} objects to filter", items.len())
+        ))
+}
+
+pub fn check_keys((request, payload): (HttpRequest<AppState>, Json<KeysRequest>)) -> impl Responder {
+    let keys = &payload.keys;
+    let filter = request.state().filter.lock().unwrap();
+    let mut response = FilteredResponse::new();
+    keys.iter().for_each(|key| {
+        if filter.get(key) {
+            response.add_matched(key);
+        } else {
+            response.add_missing(key);
+        }
+    });
+    response
+}
